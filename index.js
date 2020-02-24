86003cdf8e1efae9492f36d02f21a00b4cd9287c
Tommy <tomstuart123@hotmail.co.uk>
2020-02-24 14:28:51 -0500

    for presentation with bugs

--- index.js
+++ index.js
@@ -9,43 +9,7 @@ newsApp.currentArticles = 6;
 newsApp.pageLoad = function () {
     window.onload = function () {
         newsApp.getNewsHeadlines('top-headlines', '', 'country=us&');
-        newsApp.getTextAnalysis();
     }
-}
-
-newsApp.getTextAnalysis = () => {
-    newsApp.newsStore = [];
-    // pull main headlines
-    // const API_KEY = '4d9a7a8d7bf72865d0a6735aecde4bfc';
-    // const API_ID = '057b23f0'
-    // const baseURL = 'https://api.aylien.com/api/v1/';
-    // const articleURL = 'https://www.biography.com/news/frida-kahlo-bus-accident';
-    // const sentiment = 'sentiment?url=';
-    // let requestString = baseURL + sentiment + articleURL
-
-
-    const API_KEY = '4d9a7a8d7bf72865d0a6735aecde4bfc';
-    const API_ID = '057b23f0'
-    const baseURL = "https://api.aylien.com/api/v1/";
-    const articleURL = "https://www.biography.com/news/frida-kahlo-bus-accident";
-    const sentiment = "sentiment?url=";
-    requestString = baseURL + sentiment + articleURL;
-    const proxyUrl = "https://cors-anywhere.herokuapp.com/";
-    requestString = proxyUrl + requestString 
-    console.log(requestString)
-
-    fetch(requestString, {
-        method: "GET",
-        headers: {
-            "X-AYLIEN-TextAPI-Application-Key": API_KEY,
-            "X-AYLIEN-TextAPI-Application-ID": API_ID,
-        }
-        })
-        .then(function (data) {
-            data.json().then(data => {
-                console.log(data)
-            })
-         })
 }
 
 newsApp.getNewsHeadlines = (search='', topic='', country='') => {
@@ -80,32 +44,53 @@ newsApp.getNewsHeadlines = (search='', topic='', country='') => {
     getHeadlines()
 }
 
-newsApp.displayHeadlines = (news, articleAmount) => {
+newsApp.displayHeadlines = (news, articleAmount, sentiment) => {
     // clear old headlines 
     news.forEach((item, index) => {  
         if (index < articleAmount) {
             // Create new Div, title and Img tag and add classnames
             const newDiv = document.createElement("div");
             newDiv.className = "headlineCard";
+            const newPTag = document.createElement("p");
+            newPTag.className = "sourceInCard";
+            newPTag.id = `${item.title}`
             const newATag = document.createElement("a");
+            newATag.className = "aTagInCard";
+            newATag.id = `${item.title}`
+            newATag.className = "aTagInCard" + index;
             const imgDiv = document.createElement("div");
             imgDiv.className = "headlineImageDiv";
             const newImg = document.createElement("img");
             newImg.className = "headlineImage";
+            newImg.id = `${item.title}`
             const newH2 = document.createElement("h2");
             newH2.className = "headlineText";
+            newH2.id = `${item.title}`
+            const linkATag = document.createElement("a");
+            linkATag.className = "linkATag";
+
 
             // add news api data to the title and image tags
             const headlineText = document.createTextNode(`${item.title}`);
+            linkATag.setAttribute('href', `${item.url}`);
             newImg.setAttribute("src", `${item.urlToImage}`);
-            newATag.setAttribute('href', `${item.url}`);
+            const sourceText = document.createTextNode(`Source: ${item.source.name}`);
+            const linkText = document.createTextNode(`(go to article here)`);
+
             // append text to h2 then h2 and and Image tag to div. Then append div to section ready
+            linkATag.appendChild(linkText);
             newH2.appendChild(headlineText);
+            newPTag.appendChild(sourceText)
             imgDiv.appendChild(newImg)
             newDiv.appendChild(newATag)
             newATag.appendChild(newH2);
             newATag.appendChild(imgDiv);
-            document.querySelector('.newsCardsSection').appendChild(newDiv);  
+            newDiv.appendChild(newPTag);
+            newDiv.appendChild(linkATag);
+            document.querySelector('.newsCardsSection').appendChild(newDiv);
+
+            //finally get the sentiment of the URL process and append it to the card 
+            newsApp.checkURLForSentiment(item.url, index)  
         }
         
     })
@@ -119,11 +104,56 @@ newsApp.clearHeadlines = function() {
 } 
 
 
+newsApp.checkURLForSentiment = (url, index) => {
+
+    // pull main headlines
+    // const API_KEY = '4d9a7a8d7bf72865d0a6735aecde4bfc';
+    // const API_ID = '057b23f0'
+    // const baseURL = 'https://api.aylien.com/api/v1/';
+    // const articleURL = 'https://www.biography.com/news/frida-kahlo-bus-accident';
+    // const sentiment = 'sentiment?url=';
+    // let requestString = baseURL + sentiment + articleURL
+    const API_KEY = 'add9724d11f8001f0046cceb959d5cee';
+    const API_ID = '4ba605d0'
+    const baseURL = "https://api.aylien.com/api/v1/";
+    const articleURL = url;
+    const sentiment = "sentiment?url=";
+    requestString = baseURL + sentiment + articleURL;
+    const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+    requestString = proxyUrl + requestString
+
+    fetch(requestString, {
+        method: "GET",
+        headers: {
+            "X-AYLIEN-TextAPI-Application-Key": API_KEY,
+            "X-AYLIEN-TextAPI-Application-ID": API_ID,
+        }
+    })
+        .then(function (data) {
+            data.json().then(data => {
+                // add sentiment to the card
+                newsApp.displaySentimentOnPage(data.polarity, index)
+            })
+            
+    })
+    
+}
+
+newsApp.displaySentimentOnPage = (sentiment, index) => {
+    const newPTag = document.createElement("p");
+    newPTag.className = "sentimentInCard";
+    const headlineText = document.createTextNode(`Mood: ${sentiment}`);
+    const aTag = document.querySelector(`.aTagInCard${index}`)
+    newPTag.appendChild(headlineText);
+    aTag.appendChild(newPTag);
+} 
+
 newsApp.events = function () {
     newsApp.mobileNavClick()
     newsApp.moreArticles()
     newsApp.buttonSearch()
     newsApp.receiveSearchField()
+    newsApp.goToArticlePage()
 }
 
 newsApp.mobileNavClick = function () {
@@ -191,7 +221,29 @@ newsApp.refreshNewsOnSearch = function (searchType, searchField) {
     console.log('search')
 }
 
+newsApp.goToArticlePage = function() {
+    const link = document.querySelector('.newsCardsSection')
+    link.onclick = function (event) {
+        console.log(event.target.id)
+        var newTitle = event.target.id
+        cutTitle = newsApp.removeSourceFromTitle(newTitle)
+        console.log(cutTitle)
+        const titleForSearch = 'q=' + cutTitle + '&';
+        newsApp.refreshNewsOnSearch('everything', titleForSearch);
+    }
+    
 
+}
+
+newsApp.removeSourceFromTitle = (newTitle)=> {
+    // newTitle = newTitle.substr(0, newTitle.indexOf(' ', newTitle.indexOf(' ') + 1));
+    newTitle = newTitle.split(/\s+/).slice(0, 6).join(" ");
+    return newTitle
+}
+
+newsApp.saveToLocalStorage = function (title) {
+    localStorage.setItem('title', `${title}`)
+}
 
 
 
